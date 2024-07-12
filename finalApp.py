20491d3faad753a1946df76a695b7648ad4cf9a7
JCieF <166143233+JCieF@users.noreply.github.com>
2024-07-12 16:21:17 +0800

    Update finalApp.py

--- finalApp.py
+++ finalApp.py
@@ -5,33 +5,26 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 
-# Load the model
 model = load_model('finalExamIMG.keras')
+classes = ['Amoeba', 'Euglena', 'Hydra', 'Paramecium', 'Rod_bacteria', 'Spherical_bacteria', 'Spiral_bacteria', 'Yeast']
 
-# Function to preprocess the uploaded image
-def preprocess_image(img):
-    img = img.resize((128, 128))  # Resize to match model input
-    img_array = image.img_to_array(img)  # Convert image to array
-    img_array = np.expand_dims(img_array, axis=0) / 255.0  # Normalize
-    return img_array
-
-# Streamlit app
-st.title("Image Classification with My Model")
+st.title("Bacteria Classification")
 
 uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    # Load and preprocess the image
     img = Image.open(uploaded_file)
     array = preprocess_image(img)
-
-    # Predict the class
     prediction = model.predict(array)
-    predicted_class = np.argmax(prediction, axis=1)
-
-    # Display the image
+    predicted_class_index = np.argmax(prediction, axis=1)[0]
+    predicted_class_name = classes[predicted_class_index]  
     st.image(img, caption='Uploaded Image', use_column_width=True)
-    st.write(f'Predicted class: {predicted_class[0]}')
+    st.write(f'Predicted class: {predicted_class_name}')  
 
-    # Optionally, show the prediction probabilities
     st.write('Prediction probabilities:', prediction)
+
+def preprocess_image(img):
+    img = img.resize((128, 128))  
+    img_array = image.img_to_array(img)  
+    img_array = np.expand_dims(img_array, axis=0) / 255.0 
+    return img_array
